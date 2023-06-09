2a655d980844e504f1f39f81c69e03f78188d79a
JLoya900 <jloya7557@gmail.com>
2023-06-09 14:18:50 -0700

    Routes, model & controller, uploaded API

---
+++ utils/data.js
@@ -0,0 +1,29 @@
+const userData = [
+    {
+      user_name: 'JLoya',
+      email: 'email1@example.com',
+    },
+    {
+      user_name: 'NickAr',
+      email: 'email2@example.com',
+    },
+    {
+      user_name: 'AlanPi',
+      email: 'email3@example.com',
+    },
+    {
+      user_name: 'Maurelius',
+      email: 'email4@example.com',
+    },
+    {
+      user_name: 'SofiaAv',
+      email: 'email5@example.com',
+    },
+    {
+      user_name: 'JackL',
+      email: 'email6@example.com',
+    },
+  ]
+  
+  // Export the functions for use in seed.js
+  module.exports = { userData };
